02d4ece418793cb1f033e56ef78da68dc6d2e7a2
Isaac Priestley <progressions@gmail.com>
2018-01-09 11:14:03 -0600

    Whitespace

--- web/controllers/list_controller.ex
+++ web/controllers/list_controller.ex
@@ -17,15 +17,15 @@ defmodule Todo.ListController do
 
       render(conn, "show.json", list: list)
     else
-      :error -> malformed_request(conn)
-      nil -> not_found(conn, "List not found")
+      :error              -> malformed_request(conn)
+      nil                 -> not_found(conn, "List not found")
     end
   end
 
   def create(conn, %{"list" => %{"name" => name}}) do
-    with user           <- Todo.UserSession.current_user(conn),
-         changeset      <- Ecto.build_assoc(user, :lists, name: name),
-         {:ok, list}    <- Repo.insert(changeset) do
+    with user                     <- Todo.UserSession.current_user(conn),
+         changeset                <- Ecto.build_assoc(user, :lists, name: name),
+         {:ok, list}              <- Repo.insert(changeset) do
 
       conn
       |> put_status(201)
@@ -36,19 +36,19 @@ defmodule Todo.ListController do
   end
 
   def update(conn, %{"id" => uuid, "list" => params}) do
-    with user               <- Todo.UserSession.current_user(conn),
-         {:ok, uuid}        <- Ecto.UUID.cast(uuid),
-         list = %List{}     <- find_list(user, uuid) |> Repo.preload(:user),
-         changeset          <- List.changeset(list, params),
-         {:ok, updated}     <- Repo.update(changeset) do
+    with user                       <- Todo.UserSession.current_user(conn),
+         {:ok, uuid}                <- Ecto.UUID.cast(uuid),
+         list = %List{}             <- find_list(user, uuid) |> Repo.preload(:user),
+         changeset                  <- List.changeset(list, params),
+         {:ok, updated}             <- Repo.update(changeset) do
 
       conn
       |> put_status(201)
       |> render("update.json", list: updated)
     else
-      nil -> not_found(conn, "List not found")
-      :error -> malformed_request(conn)
-      {:error, %{errors: errors}} -> errors(conn, errors)
+      nil                           -> not_found(conn, "List not found")
+      :error                        -> malformed_request(conn)
+      {:error, %{errors: errors}}   -> errors(conn, errors)
     end
   end
 
@@ -62,8 +62,8 @@ defmodule Todo.ListController do
       |> put_status(204)
       |> send_resp(:no_content, "")
     else
-      :error -> malformed_request(conn)
-      nil -> not_found(conn, "List not found")
+      :error                -> malformed_request(conn)
+      nil                   -> not_found(conn, "List not found")
     end
   end
 
