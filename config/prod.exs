ea4f4407ba4b3cb066db828dcb24a03dc814abb2
Isaac Priestley <progressions@gmail.com>
2018-01-15 16:50:45 -0600

    Fix config

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :todo, Todo.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
-config :hello, Todo.Repo,
+config :todo, Todo.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
