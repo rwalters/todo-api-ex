ba7141332876b414097fe3033a092f0fb488b2fb
Isaac Priestley <progressions@gmail.com>
2018-01-18 21:56:16 -0600

    View test

---
+++ test/todo_web/views/item_view_test.exs
@@ -0,0 +1,14 @@
+defmodule TodoWeb.ItemViewTest do
+  use TodoWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  setup do
+    item = %Todo.Item{name: "buy milk", list_id: "123-abc", id: "987-zyx"}
+    {:ok, item: item}
+  end
+
+  test "renders single item", state do
+    assert render(TodoWeb.ItemView, "show.json", item: state.item) == %{finished_at: nil, id: "987-zyx", name: "buy milk", src: "http://localhost:4000/lists/123-abc/items/987-zyx"}
+  end
+end
