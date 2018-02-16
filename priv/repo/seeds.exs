95dea79035836ca2d4bdc849747a362bb2e71992
Isaac Priestley <progressions@gmail.com>
2018-02-16 14:02:05 -0500

    Create seed to build default user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+username = "username"
+password = "password"
+
+Todo.User.changeset(%Todo.User{}, %{username: username, password: password})
+|> Todo.Repo.insert!()
