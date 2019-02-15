0ab1dfdce91f7e63e57fa47907562070a548a741
Isaac Priestley <progressions@gmail.com>
2019-02-15 15:42:09 -0500

    Add Poison to the mix

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Todo.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:timex, "~> 3.1"}
+      {:timex, "~> 3.1"},
+      {:poison, "~> 3.1"},
     ]
   end
 
