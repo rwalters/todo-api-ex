5eff91a45543ff0326d900ff0bf0cd392604beb4
Isaac Priestley <progressions@gmail.com>
2019-02-15 17:11:18 -0500

    Update cowboy to plug_cowboy 2.0

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Todo.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
       {:timex, "~> 3.1"},
       {:poison, "~> 3.1"}
     ]
